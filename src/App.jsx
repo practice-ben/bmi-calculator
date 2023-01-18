4dbc83a8eaca88f9f21834e013aa137f569e4518
Benson Njuguna <njugunab655@gmail.com>
2023-01-18 20:37:37 +0300

    Add comments

--- src/App.jsx
+++ src/App.jsx
@@ -24,26 +24,30 @@ export default function App() {
   return (
     <div className="container">
       <h1>Calculate Body Mass Index</h1>
+
+      {/* input data */}
       <div className="inputs">
+        {/* weight */}
         <div className="weight">
           <h4>Weight</h4>
-          <input type="range" min="0" max="700" onChange={displayWeight} value={weight} />
+          <input type="range" min="1" max="700" onChange={displayWeight} value={weight} />
           <p>
             <span className="bold">{weight}</span>
             Kilograms
           </p>
         </div>
+        {/* height */}
         <div className="height">
           <h4>Height</h4>
-          <input type="range" min="0" max="3" step="0.01" onChange={displayHeight} value={height} />
+          <input type="range" min="0.1" max="3" step="0.01" onChange={displayHeight} value={height} />
           <p>
             <span className="bold">{height}</span>
             Meters
           </p>
         </div>
 
+        {/* display the BMI */}
         <p className="bmi">{bmi}</p>
-
       </div>
     </div>
   )
