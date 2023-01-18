01faea098dffc202cbf3b92d9eb8327f18bc43e7
Benson Njuguna <njugunab655@gmail.com>
2023-01-18 20:32:57 +0300

    Add function to calculate bmi

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,48 @@ import {useState, useMemo} from "react";
 export default function App() {
   const [weight, setWeight] = useState(0);
   const [height, setHeight] = useState(0);
+
+
   function displayWeight(e) {
     const kilos = e.target.value;
     setWeight(kilos)
   }
+
   function displayHeight(e) {
     const long = e.target.value;
     setHeight(long)
   }
+
+  function calculateBmi(w, h) {
+    return w / (h * h);
+  }
+
+  const bmi = useMemo(() => calculateBmi(weight, height), [weight, height]);
+
   return (
     <div className="container">
       <h1>Calculate Body Mass Index</h1>
-      <h4>Weight</h4>
-      <input type="range" min="0" max="700" onChange={displayWeight} value={weight} />
-      <p>{weight}</p>
-      <h4>Height</h4>
-      <input type="range" min="0" max="3" step="0.01" onChange={displayHeight} value={height} />
-      <p>{height}</p>
+      <div className="inputs">
+        <div className="weight">
+          <h4>Weight</h4>
+          <input type="range" min="0" max="700" onChange={displayWeight} value={weight} />
+          <p>
+            <span className="bold">{weight}</span>
+            Kilograms
+          </p>
+        </div>
+        <div className="height">
+          <h4>Height</h4>
+          <input type="range" min="0" max="3" step="0.01" onChange={displayHeight} value={height} />
+          <p>
+            <span className="bold">{height}</span>
+            Meters
+          </p>
+        </div>
+
+        <p className="bmi">{bmi}</p>
+
+      </div>
     </div>
   )
 }
