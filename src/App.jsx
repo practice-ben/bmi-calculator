e9582867f44a3d98e2a9f27735d66c78d24e539c
Benson Njuguna <njugunab655@gmail.com>
2023-01-18 16:18:45 +0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import {useState, useMemo} from "react";
+
+export default function App() {
+  return (
+    <div className="container">
+      <h1>Calculate Body Mass Index</h1>
+    </div>
+  )
+}
