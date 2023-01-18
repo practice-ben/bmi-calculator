56da9ee3b6817bf41734b6f823666d96a8c98062
Benson Njuguna <njugunab655@gmail.com>
2023-01-18 20:20:21 +0300

    Add range elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import {useState, useMemo} from "react";
 
 export default function App() {
+  const [weight, setWeight] = useState(0);
+  const [height, setHeight] = useState(0);
+  function displayWeight(e) {
+    const kilos = e.target.value;
+    setWeight(kilos)
+  }
+  function displayHeight(e) {
+    const long = e.target.value;
+    setHeight(long)
+  }
   return (
     <div className="container">
       <h1>Calculate Body Mass Index</h1>
+      <h4>Weight</h4>
+      <input type="range" min="0" max="700" onChange={displayWeight} value={weight} />
+      <p>{weight}</p>
+      <h4>Height</h4>
+      <input type="range" min="0" max="3" step="0.01" onChange={displayHeight} value={height} />
+      <p>{height}</p>
     </div>
   )
 }
